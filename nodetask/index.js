9e2c44c985220b863ced5657beca60264bccee40
freni123 <frenivaghasiya@gmail.com>
2023-07-14 08:30:25 +0530

    comm

---
+++ nodetask/index.js
@@ -0,0 +1,5 @@
+console.log("like");
+var a = 10;
+var b = 20;
+var c = 40;
+console.log(a+b+c);
---
+++ nodetask/newindex.js
@@ -0,0 +1,3 @@
+const fs =require('fs');
+// console.log("Hello How are you?");
+fs.writeFileSync("hello.txt","kem cho ?")
